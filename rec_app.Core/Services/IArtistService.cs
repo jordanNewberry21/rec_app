52f41f05012c3a7286e863472461047556185bbe
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-17 12:27:43 -0500

    added music config file to build the music entity type

--- rec_app.Core/Services/IArtistService.cs
+++ rec_app.Core/Services/IArtistService.cs
@@ -1,7 +1,15 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using rec_app.Core.Models;
+
 namespace rec_app.Core.Services
 {
     public interface IArtistService
     {
+        Task<IEnumerable<Artist>> GetAllArtists();
+        Task<Artist> GetArtistById(int id);
+        Task<Artist> CreateArtist(Artist newArtist);
+        Task UpdateArtist(Artist artistToBeUpdated, Artist artist);
+        Task DeleteArtist(Artist artist);
     }
 }

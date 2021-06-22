802ebdc2bc98558e7d84fe5dc601f051fbe3c76c
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-22 15:21:23 -0500

    added music repository

---
+++ rec_app.Data/Repositories/MusicRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using rec_app.Core.Models;
+using rec_app.Core.Repositories;
+
+namespace rec_app.Data.Repositories
+{
+    public class MusicRepository : Repository<Music>, IMusicRepository
+    {
+        public MusicRepository(RecAppDbContext context)
+            : base(context)
+        { }
+
+        public async Task<IEnumerable<Music>> GetAllWithArtistAsync()
+        {
+            return await RecAppDbContext.Musics
+                .Include(m => m.Artist)
+                .ToListAsync();
+        }
+
+        public async Task<Music> GetWithArtistByIdAsync(int id)
+        {
+            return await RecAppDbContext.Musics
+                .Include(m => m.Artist)
+                .SingleOrDefaultAsync(m => m.Id == id); ;
+        }
+
+        public async Task<IEnumerable<Music>> GetAllWithArtistByArtistIdAsync(int artistId)
+        {
+            return await RecAppDbContext.Musics
+                .Include(m => m.Artist)
+                .Where(m => m.ArtistId == artistId)
+                .ToListAsync();
+        }
+
+        private RecAppDbContext RecAppDbContext
+        {
+            get { return Context as RecAppDbContext; }
+        }
+    }
+}

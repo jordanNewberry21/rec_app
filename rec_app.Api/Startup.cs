8775b523f7716d7136be57cb6f006c9037b17300
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-22 15:37:23 -0500

    adding scoped dependancy injection to tie application logic together

--- rec_app.Api/Startup.cs
+++ rec_app.Api/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using rec_app.Core;
+using rec_app.Data;
 
 namespace rec_app.Api
 {
@@ -32,6 +34,7 @@ namespace rec_app.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "rec_app.Api", Version = "v1" });
             });
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

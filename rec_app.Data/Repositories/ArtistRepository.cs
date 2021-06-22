2f9c8650ac6cabf6e6fcd560dc30bdde830f0f20
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-22 15:25:21 -0500

    added artist repository and fixed small error

---
+++ rec_app.Data/Repositories/ArtistRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using rec_app.Core.Models;
+using rec_app.Core.Repositories;
+
+namespace rec_app.Data.Repositories
+{
+    public class ArtistRepository : Repository<Artist>, IArtistRepository
+    {
+        public ArtistRepository(RecAppDbContext context)
+            : base(context)
+        { }
+
+        public async Task<IEnumerable<Artist>> GetAllWithMusicAsync()
+        {
+            return await RecAppDbContext.Artists
+                .Include(a => a.Musics)
+                .ToListAsync();
+        }
+
+        public Task<Artist> GetWithMusicsByIdAsync(int id)
+        {
+            return RecAppDbContext.Artists
+                .Include(a => a.Musics)
+                .SingleOrDefaultAsync(a => a.Id == id);
+        }
+
+        private RecAppDbContext RecAppDbContext
+        {
+            get { return Context as RecAppDbContext; }
+        }
+    }
+}

1bf99f300b7b7895bccb50484d63dbd223bda436
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-25 13:21:33 -0500

    added services to connect to the postgres database

--- rec_app.Api/Startup.cs
+++ rec_app.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,7 +38,7 @@ namespace rec_app.Api
             });
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddDbContext<RecAppDbContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("Default")));
+                options.UseNpgsql(Configuration.GetConnectionString("Default"), x => x.MigrationsAssembly("rec_app.Data")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

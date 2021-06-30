296303a20a895ca7cf904c2a0a312588a039c8b6
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-30 15:45:30 -0500

    hit a good stopping point for today

--- rec_app.Api/Startup.cs
+++ rec_app.Api/Startup.cs
@@ -1,20 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
 using rec_app.Core;
 using rec_app.Data;
+using rec_app.Core.Services;
+using rec_app.Services;
 
 namespace rec_app.Api
 {
@@ -39,6 +33,8 @@ namespace rec_app.Api
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddDbContext<RecAppDbContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("Default"), x => x.MigrationsAssembly("rec_app.Data")));
+            services.AddTransient<IMusicService, MusicService>();
+            services.AddTransient<IArtistService, ArtistService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

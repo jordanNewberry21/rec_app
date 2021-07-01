7a79d6ec6577a7333e600d3c568c2ea19eba31e4
Jordan Newberry <jordan.newberry@gmail.com>
2021-07-01 16:22:58 -0500

    created get route for getAllMusics but it's not working. im close though

---
+++ rec_app.Api/Controllers/MusicsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using rec_app.Core.Models;
+using rec_app.Core.Services;
+
+namespace rec_app.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicsController : ControllerBase
+    {
+        [HttpGet("")]
+        public async Task<ActionResult<IEnumerable<Music>>> GetAllMusics()
+        {
+            var musics = await _musicService.GetAllWithArtist();
+            return Ok(musics);
+        }
+            private readonly IMusicService _musicService;
+
+            public MusicsController(IMusicService musicService, IMapper mapper)
+            {
+                this._musicService = musicService;
+            }
+        }
+    }
+

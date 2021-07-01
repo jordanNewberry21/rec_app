2c962e1d3c4cc15d99890d75d586687838eb0b2e
Jordan Newberry <jordan.newberry@gmail.com>
2021-07-01 16:41:18 -0500

    added mapping profile. almost there

---
+++ rec_app.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using rec_app.Api.Resources;
+using rec_app.Core.Models;
+
+namespace rec_app.Api.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Resource
+            CreateMap<Music, MusicResource>();
+            CreateMap<Artist, ArtistResource>();
+
+            // Resource to Domain
+            CreateMap<MusicResource, Music>();
+            CreateMap<ArtistResource, Artist>();
+        }
+    }
+}

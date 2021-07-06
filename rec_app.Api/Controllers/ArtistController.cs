924a93bed98e7455f97c677d2416576c2ca1b7e6
Jordan Newberry <jordan.newberry@gmail.com>
2021-07-06 15:59:17 -0500

    creating artists controller for artist related endpoints

---
+++ rec_app.Api/Controllers/ArtistController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using rec_app.Api.Resources;
+using rec_app.Api.Validators;
+using rec_app.Core.Models;
+using rec_app.Core.Services;
+
+namespace rec_app.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistsController : ControllerBase
+    {
+        private readonly IArtistService _artistService;
+        private readonly IMapper _mapper;
+
+        public ArtistsController(IArtistService artistService, IMapper mapper)
+        {
+            this._mapper = mapper;
+            this._artistService = artistService;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<IEnumerable<ArtistResource>>> GetAllArtists()
+        {
+            var artists = await _artistService.GetAllArtists();
+            var artistResources = _mapper.Map<IEnumerable<Artist>, IEnumerable<ArtistResource>>(artists);
+
+            return Ok(artistResources);
+        }
+    }
+}
